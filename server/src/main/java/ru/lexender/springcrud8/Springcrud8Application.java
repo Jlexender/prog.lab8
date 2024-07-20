f44a6348c30d19568605a0b5d511c026b84933a8
LLexender <aleksandrdmitryevich@gmail.com>
2024-07-20 21:57:39 +0300

    Added test workflows

--- server/src/main/java/ru/lexender/springcrud8/Springcrud8Application.java
+++ server/src/main/java/ru/lexender/springcrud8/Springcrud8Application.java
@@ -3,6 +3,8 @@ package ru.lexender.springcrud8;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.stream.Stream;
+
 @SpringBootApplication
 public class Springcrud8Application {
 
